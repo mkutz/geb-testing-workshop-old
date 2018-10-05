0798cfa5145fb7099529e40fd98bee24866cd0d0
Michael Kutz <michael.kutz@rewe-digital.com>
2018-10-05 17:20:45 +0200

    Solve task 2.1
    Create a `GoogleStartPage` class containing the URL. Now change your
    features to use the `to` instead of `go` for navigating to the page.

--- src/test/groovy/de/assertagile/workshop/gebtesting/test/GoogleUiSpec.groovy
+++ src/test/groovy/de/assertagile/workshop/gebtesting/test/GoogleUiSpec.groovy
@@ -1,13 +1,13 @@
 package de.assertagile.workshop.gebtesting.test
 
-
+import de.assertagile.workshop.gebtesting.test.pages.GoogleStartPage
 import geb.spock.GebSpec
 
 class GoogleUiSpec extends GebSpec {
 
     def "the Google logo can be found on the Google start page"() {
         when:
-        go("https://google.com")
+        to(GoogleStartPage)
 
         then:
         $("#hplogo")
@@ -15,7 +15,7 @@ class GoogleUiSpec extends GebSpec {
 
     def "the main search input field can be found"() {
         when:
-        go("https://google.com")
+        to(GoogleStartPage)
 
         then:
         $("#lst-ib")
---
+++ src/test/groovy/de/assertagile/workshop/gebtesting/test/pages/GoogleStartPage.groovy
@@ -0,0 +1,8 @@
+package de.assertagile.workshop.gebtesting.test.pages
+
+import geb.Page
+
+class GoogleStartPage extends Page {
+
+    static url = "https://google.com"
+}
